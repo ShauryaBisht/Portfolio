42ca7c450dae73195247856564c68be719b94b05
ShauryaBisht <shauryabisht707@gmail.com>
2026-04-26 19:44:55 +0530

    Navbar done

---
+++ my-portfolio/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { Space_Grotesk } from "next/font/google";
+
+const spaceGrotesk = Space_Grotesk({
+    subsets: ["latin"],
+    variable: "--font-space-grotesk",
+})
+
+export function Navbar() {
+    return (
+        <header className="flex h-[80.8px] w-full items-center justify-between px-12 bg-[#0A0A0A]/80 border-b border-white/5 backdrop-blur-md sticky top-0 z-50">
+
+            {/* Left Side: Logo */}
+            <div className="flex items-center gap-1 font-space">
+                <span className={`text-[#00F5FF] ${spaceGrotesk.className} text-[24px] font-bold leading-8`}>
+          //
+                </span>
+                <p className={`text-[24px] text-white font-bold leading-8 ${spaceGrotesk.className} m-0`}>
+                    BUILD_LOG
+                </p>
+            </div>
+
+
+            <nav className={`${spaceGrotesk.className} flex items-center gap-8 font-space `}>
+                <ul className="flex list-none gap-8 p-0 m-0 text-[14px] font-medium tracking-widest text-gray-300 uppercase">
+                    <li className="hover:text-[#00F5FF] text-[#E5E7EB] cursor-pointer transition-colors">Projects</li>
+                    <li className="hover:text-[#FF5CFF] cursor-pointer text-[#E5E7EB] transition-colors">Skills</li>
+                    <li className="hover:text-[#E5FF00] text-[#E5E7EB] cursor-pointer transition-colors">Contact</li>
+                </ul>
+
+                {/* Resume Button */}
+                <button className="
+
+  px-8 py-3 
+  border-[0.8px] border-[#00F5FF] 
+  text-[#00F5FF] 
+  font-space font-bold uppercase tracking-widest 
+  bg-transparent 
+  transition-all duration-300 
+  rounded-none
+  hover:text-black
+  hover:bg-[#00F5FF] 
+  hover:shadow-[0_0_15px_rgba(0,245,255,0.1)]
+">
+                    Resume
+                </button>
+            </nav>
+        </header>
+    )
+}
